09d503ce6dff441ca7536291ca329477c63f28fb
Yeqin Gong <gon@gyeq.in>
2025-11-08 23:16:53 +0800

    更新信息和编译配置

--- build.config.js
+++ build.config.js
@@ -1,7 +1,11 @@
 import { resolve } from 'node:path';
+import copy from 'bun-copy-plugin';
 
-const srcDir = resolve(import.meta.dir, 'src');
+const projectDir = import.meta.dir;
+const srcDir = resolve(projectDir, 'src');
+const distDir = resolve(projectDir, 'dist');
 
 export default {
   entrypoints: [resolve(srcDir, 'index.jsx'), resolve(srcDir, 'info.jsx')],
+  plugins: [copy(`${resolve(Bun.resolveSync('vditor', projectDir), '..')}/`, resolve(distDir, 'vditor/dist'))],
 };
